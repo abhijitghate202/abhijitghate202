ec1f8477eab8c3524ef46806a4f55fb17b23e259
Abhijit Ghate <abhijitghate202@gmail.com>
2022-11-17 17:16:35 +0530

    Account

---
+++ encapsulation/Account.java
@@ -0,0 +1,42 @@
+package encapsulation;
+
+public class Account {
+    private int accountNo;
+    private String name;
+    private Double amount;
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
+class accountImpl{
+    public static void main(String[] args) {
+        Account account = new Account();
+        account.setAccountNo(89503);
+        account.setAmount(55499.5);
+        account.setName("Abhijit Ghate");
+        System.out.println("Account Number : " + account.getAccountNo());
+        System.out.println("Amount : " + account.getAmount());
+        System.out.println("Name : " + account.getName());
+    }
+}
