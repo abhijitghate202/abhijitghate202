7d7ff0791ce7f3ba27ee4ef044a817616987ec87
Abhijit Ghate <abhijitghate202@gmail.com>
2022-11-24 16:14:43 +0530

    Number of all the items in Array

---
+++ arrays/NumberofElements.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class NumberofElements {
+    public static void main(String[] args) {
+        int[] array = new int[]{10, 20, 30, 40, 50, 60, 70};
+        System.out.println("Number of Elements present in array : " + array.length);
+    }
+}
