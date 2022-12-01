f456a4383b08da31d0d5fe18e5c191061fd07993
Abhijit Ghate <abhijitghate202@gmail.com>
2022-12-01 16:59:33 +0530

    Java String

---
+++ javastring/JavaStringDemo.java
@@ -0,0 +1,16 @@
+package javastring;
+
+public class JavaStringDemo {
+    public static void main(String[] args) {
+        char ch[] = {'W', 'E', 'L', 'C', 'O', 'M', 'E'};
+        System.out.println(ch);
+
+        String s1 = new String("WELCOME");
+        System.out.println(s1);
+
+        String s2 = "WELCOME";
+        System.out.println(s2);
+
+
+    }
+}
