ae75882ba9ac379b62b3b8d375f7fcdb48a52794
Abhijit Ghate <abhijitghate202@gmail.com>
2022-12-19 17:21:16 +0530

    Exception Handling arithmatic handling

--- exceptionhandling/ArithmaticExceptionDemo.java
+++ exceptionhandling/ArithmaticExceptionDemo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ArithmaticExceptionDemo {
     public static void main(String[] args) {
-        Scanner s1 = new Scanner(System.in);
-        System.out.println(5 / s1.nextInt());
+        int a = 50/0;
+        System.out.println(a);
     }
 }
