d7c0f07faeb060d8e8bf86ac433859124b805613
Abhijit Ghate <abhijitghate202@gmail.com>
2022-12-23 14:50:49 +0530

    Finally, throw

--- exceptionhandling/FileNotFoundExceptionDemo.java
+++ exceptionhandling/FileNotFoundExceptionDemo.java
@@ -3,14 +3,14 @@ package exceptionhandling;
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 
-public class FilenotFoundException extends Throwable {
+public class FileNotFoundExceptionDemo {
     public static void main(String[] args) {
         try {
             PrintWriter printwriter = new PrintWriter("input.txt");
             printwriter.print("Welcome");
             printwriter.close();
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println(e);
         }
         System.out.println("File Saved");
     }
---
+++ exceptionhandling/VaccineCriteriaDemo.java
@@ -0,0 +1,15 @@
+package exceptionhandling;
+
+import java.util.Scanner;
+
+public class VaccineCriteriaDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer age = sc.nextInt();
+        if (age < 60) {
+            throw new ArrayIndexOutOfBoundsException("Not Eligible");
+        } else {
+            System.out.println("Eligible");
+        }
+    }
+}
