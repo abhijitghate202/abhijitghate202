2b6765856c5d38d1877e7c68ecfd8e6b30a38230
Abhijit Ghate <abhijitghate202@gmail.com>
2023-02-22 17:06:39 +0530

    Priority & queue

---
+++ Collections/PriorityDemo.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityDemo {
+    public static void main(String[] args) {
+        Queue<Integer> numbers = new PriorityQueue<>();
+        numbers.offer(50);
+        numbers.offer(10);
+        numbers.offer(20);
+        numbers.offer(80);
+        numbers.offer(30);
+        numbers.offer(60);
+        System.out.println(numbers);
+        System.out.println(numbers.peek());
+
+    }
+}
